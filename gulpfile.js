b081652738d840243f491650fc6868866c17a8a7
sunzhe <davidsunsz@hotmail.com>
2016-05-21 17:36:52 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-   gulp.run('sass');
+gulp.task('default', ['auto']);
+
+gulp.task('css', function () {
+    gulp.src('css/*.css')
+        .pipe(concat('abpPlayer.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('sass', function() {
-    gulp.src('./css/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'));
+gulp.task('js', function () {
+    gulp.src('js/*.js')
+        .pipe(concat('abpPlayer.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
 });
 
-gulp.watch('css/*.scss', ['sass']);
+gulp.task('copyFont', function () {
+    gulp.src('fonts/*.*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('auto', function() {
+  gulp.watch('css/*.css', ['css']);
+  gulp.watch('js/*.js', ['js']);
+  gulp.watch('fonts/*.*', ['copyFont']);
+});
